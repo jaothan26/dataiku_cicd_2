d82ff8d644060d72beec87947f63aab976138d78
jaothan26 <44733945+jaothan26@users.noreply.github.com>
2021-04-26 15:40:36 +0200

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+    environment {
+        bundle_name = "${sh(returnStdout: true, script: 'echo "bundle_`date +%Y-%m-%d_%H-%m-%S`"').trim()}"
+    }
+    stages {
+        stage('PREPARE'){
+            steps {
+                sh 'echo ${bundle_name}'
+                git credentialsId: "git_hub_ssh", url: "git@github.com:fsergot/dss_pipeline.git"
+                sh "sed -i 's|@DESIGN_URL@|${DESIGN_URL}|' requirements.txt"
+                sh "cat requirements.txt"
+                sh "printenv"
+            }
+        }
+        }
+        }
